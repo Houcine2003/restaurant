d319b797fc09b698e987ab4a90e20f2fa165fdd2
Houcine2003 <el.houcine.afkir.2003@gmail.com>
2025-04-26 17:16:07 +0100

    Premier envoi du projet

---
+++ src/main/java/com/houcine/Restauramt/RestauramtApplication.java
@@ -0,0 +1,13 @@
+package com.houcine.Restauramt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauramtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauramtApplication.class, args);
+	}
+
+}
